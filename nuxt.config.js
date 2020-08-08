698eef0fd28f8e3fabf2644327046e4e4c9e864c
uta-mori <mori_yuta@retail-ai.jp>
2020-08-08 16:07:36 +0900

    これはどうだ

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,6 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
-  generate: {
-    fallback: '404.html', // for Netlify
-    routes: ['/nuxtdocs/'] // give the first url to start crawling
-  },
   router: {
     base: '/nuxtdocs/'
   }
