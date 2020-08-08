160efd3d20954b0d6096cd918f58c30deec1aaf6
uta-mori <mori_yuta@retail-ai.jp>
2020-08-08 15:59:56 +0900

    /忘れ

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,6 @@ import theme from '@nuxt/content-theme-docs'
 export default theme({
   generate: {
     fallback: '404.html', // for Netlify
-    routes: ['/nuxtdocs'] // give the first url to start crawling
+    routes: ['/nuxtdocs/'] // give the first url to start crawling
   }
 })
